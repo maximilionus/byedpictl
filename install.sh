84f427cbe94421dbcf03116a35735533a2541f75
maximilionus <maximilionuss@gmail.com>
2024-11-11 20:29:35 +0300

    Basic color highlighting for install/uninstall

--- install.sh
+++ install.sh
@@ -4,20 +4,22 @@ set -e
 CONF_DIR="/etc/byedpi"
 BIN_DIR="/usr/local/bin"
 
+GREEN="\033[0;32m"
+NC="\033[0m"
+
+
 TMP_DIR=$( mktemp -d )
 if [[ ! -d $TMP_DIR ]]; then
-    echo "[!] Failed to initialize temporary directory."
+    printf "[!] Failed to initialize temporary directory.\n"
     exit 1
 fi
 trap 'rm -rf -- "$TMP_DIR" && echo "Temporary directory $TMP_DIR wiped."' EXIT
 
-echo
-echo "Preparing..."
+printf "\n${GREEN}Preparing...${NC}\n"
 mkdir -vp "$CONF_DIR"
 id -u byedpi &>/dev/null || useradd -r -s /bin/false byedpi
 
-echo
-echo "Downloading dependencies..."
+printf "\n${GREEN}Downloading dependencies...${NC}\n"
 curl -L -o "$TMP_DIR/ciadpi.tar.gz" \
     "https://github.com/hufrea/byedpi/releases/download/v0.15/byedpi-15-x86_64.tar.gz"
 cd "$TMP_DIR"
@@ -30,7 +32,9 @@ curl -L -o "$BIN_DIR/hev-socks5-tunnel" \
     "https://github.com/heiher/hev-socks5-tunnel/releases/download/2.7.4/hev-socks5-tunnel-linux-x86_64"
 chmod +x "$BIN_DIR/hev-socks5-tunnel"
 
-echo
-echo "Installing..."
+printf "\n${GREEN}Installing...${NC}\n"
 cp -v hev-socks5-tunnel.yaml "$CONF_DIR"
 cp -v byedpictl.sh "$BIN_DIR/byedpictl"
+
+printf "\n${GREEN}Installation complete.${NC}\n"
+printf "Access by calling:\n    $ byedpictl help\n"
--- uninstall.sh
+++ uninstall.sh
@@ -4,12 +4,14 @@ set -e
 CONF_DIR="/etc/byedpi"
 BIN_DIR="/usr/local/bin"
 
+GREEN="\033[0;32m"
+NC="\033[0m"
 
-echo "Begin uninstall..."
+
+printf "${GREEN}Begin uninstall...${NC}\n"
 rm -rfv "$CONF_DIR"
 rm -fv "$BIN_DIR/ciadpi"
 rm -fv "$BIN_DIR/hev-socks5-tunnel"
 id -u byedpi &>/dev/null && userdel byedpi
 
-echo
-echo "Successfully uninstalled"
+printf "\n${GREEN}Successfully uninstalled.${NC}\n"
